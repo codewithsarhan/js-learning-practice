5e5d27f3f26bb9847e190007a495cfa2a2cd62ca
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-08-24 08:34:12 +0500

    Learn Remove Property In Js

---
+++ jslearning/script.js
@@ -0,0 +1,7 @@
+let para = document.querySelector(".para");
+para.remove()
+
+let check = document.querySelector(".para")
+console.log(check);
+
+// Learn Remove To Remove any element in html dom structure.
