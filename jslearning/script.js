aba033e7fb694486bea6b025fa17c2441bb0a9c0
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-08-24 08:48:11 +0500

    Learn New Tag Concept Classlist Remove

--- jslearning/script.js
+++ jslearning/script.js
@@ -7,3 +7,10 @@ let newDiv = document.querySelector(".shadow");
 console.log(newDiv);
 
 // Classlist add use to add new class without remove old class
+
+let remove = newDiv.classList.remove("shadow");
+
+console.log(remove);
+console.log(div);
+
+// CLass List Remove To Use remove any class without removing old class
