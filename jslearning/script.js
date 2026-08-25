63480c5db951f7e56f4c2cbeb5fa5e20c01b1b6a
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-08-25 09:18:20 +0500

    learning phase

--- jslearning/script.js
+++ jslearning/script.js
@@ -1,7 +1,5 @@
-let img = document.querySelector("#myPic");
-let attr  = img.getAttribute("src");
-console.log(attr);
-img.setAttribute("src" , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1nV5HX-sT5M9Magwp8s7VtxN8E54DIXLOo-y-dbG4Rw&s=10");
-let getattr = img.getAttribute("src");
-console.log(getattr);
+let para = document.querySelector("#heading");
 
+para.style.color = "yellow";
+para.style.backgroundColor = "black";
+para.style.fontSize = "25px";
