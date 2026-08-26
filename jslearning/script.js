2a1758efa6ea6271b464e0478ce661233cfdeb73
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-08-26 09:00:42 +0500

    Learning Phase

--- jslearning/script.js
+++ jslearning/script.js
@@ -2,15 +2,13 @@ let btn = document.createElement("button");
 btn.innerText = "Click Me";
 document.body.append(btn)
 
-btn.addEventListener( "click" ,  () => {
 
-btn.style.backgroundColor = "Green" ;
-btn.style.color = "white" ;
-btn.style.fontSize = "20px" ;
-btn.style.padding = "20px" ;
-btn.style.border = "none" ;
-    
+const handler = () =>{
 
-}
+btn.style.backgroundColor = "black"
 
-)
+};
+
+
+btn.addEventListener("click" , handler);
+btn.removeEventListener("click" , handler)
