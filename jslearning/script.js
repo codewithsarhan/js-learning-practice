a627ad1d77b4ec6381977bc7db5364fed60bda77
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-08-26 12:22:02 +0500

    Js Learning

--- jslearning/script.js
+++ jslearning/script.js
@@ -1,14 +1,20 @@
-let btn = document.createElement("button");
-btn.innerText = "Click Me";
-document.body.append(btn)
+let magicBtn = document.createElement("button");
+let body = document.querySelector("body");
+document.body.append(magicBtn);
+magicBtn.innerText = "Click Me";
+magicBtn.style.padding = "50px";
+magicBtn.style.border = "20px solid black";
+magicBtn.style.backgroundColor = "blue";
+magicBtn.style.color = "white";
+magicBtn.style.fontSize = "50px";
+magicBtn.style.margin = "50px";
 
+body.style.display = "flex";
+body.style.justifyContent = "center";
 
-const handler = () =>{
-
-btn.style.backgroundColor = "black"
-
+const magicBtnMagic = () => {
+  magicBtn.style.backgroundColor = "green";
+  magicBtn.innerText = "You CLick Me";
 };
 
-
-btn.addEventListener("click" , handler);
-btn.removeEventListener("click" , handler)
+magicBtn.addEventListener("click", magicBtnMagic);
