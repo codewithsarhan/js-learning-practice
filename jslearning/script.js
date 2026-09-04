731e376a10811f1834c163de61b69ef19cad85bc
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-04 09:14:46 +0500

    js learning

--- jslearning/script.js
+++ jslearning/script.js
@@ -1,39 +1,6 @@
-let mainInput = document.querySelector(".mainInput");
-let prioritydrpdwn = document.querySelector(".priyDrpdwn");
-let addTask = document.querySelector(".addtaskBtn");
-let filterdrpdwn = document.querySelector(".fliter");
-let filterOption = document.querySelector(".filterOption");
+const myName = "sarhan ali ansari";
+let myAge = 16 ;
+let collage = "ALjaddon ziauddin collage";
+let course = "Mern stack web app development";
 
-
-let task = [
-
-    
-]
-
-
-
-addTask.addEventListener( "click" , () => {
-
-let priority = prioritydrpdwn.value;
-let selectOption = filterOption.value;
-let myInput = mainInput.value ;
-
-const taskData = {
-
-date : Date.now(),
-id : crypto.randomUUID(),
-task : myInput ,
-priorityTask : priority ,
-SelectedOption : selectOption ,
-
-}
-
-task.push(taskData);
-
-
-console.log(task);
-
-
-})
-
-console.log(task);
+console.table([myName , myAge , collage , course]);
