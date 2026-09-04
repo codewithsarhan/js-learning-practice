0cb745b7e1072d901e50ba200f59b23987bbdc96
Sarhan Ali Ansari <sarhanaliansari7@gmail.com>
2026-09-04 12:32:22 +0500

    dark light mode toggle

--- jslearning/script.js
+++ jslearning/script.js
@@ -1,6 +1,27 @@
-const myName = "sarhan ali ansari";
-let myAge = 16 ;
-let collage = "ALjaddon ziauddin collage";
-let course = "Mern stack web app development";
+let toggleThemeBtn = document.querySelector(".toggleThemeBtn");
+let body = document.querySelector("body");
+let modeHeading = document.querySelector(".modeHeading");
+let countertext = document.querySelector(".countertext");
 
-console.table([myName , myAge , collage , course]);
+let initialCount = 0;
+
+let backgroundColor = "light";
+
+toggleThemeBtn.addEventListener("click", () => {
+  if (backgroundColor === "light") {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    modeHeading.innerText = "Current Theme: Dark";
+    backgroundColor = "dark";
+  } else if (backgroundColor === "dark") {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    modeHeading.innerText = "Current Theme: light";
+    backgroundColor = "light";
+  }
+
+  initialCount++;
+  console.log(initialCount);
+
+  countertext.innerText = `Change Theme ${initialCount} times`;
+});
